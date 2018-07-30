24815828fb50f5497af73c1362221c161dfd2fd9
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-07-30 11:49:27 -0600

    fixing error message and id

--- version-1/app.js
+++ version-1/app.js
@@ -23,8 +23,7 @@ app.get("/", (req, res) => {
 app.get("/:id", (req, res) => {
     var record = findById(cohorts, req.params.id);
     if (!record) {
-        res.status = 404;
-        res.json({
+        res.status(404).json({
             error: {
                 message: "No record found!"
             }
